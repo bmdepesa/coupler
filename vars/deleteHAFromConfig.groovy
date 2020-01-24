7bb7c15dd5c211737ef9560cfd0e0adba41921f6
Brandon DePesa <brandon@rancher.com>
2020-01-24 14:37:51 -0700

    improve error handling

--- vars/deleteHAFromConfig.groovy
+++ vars/deleteHAFromConfig.groovy
@@ -2,10 +2,15 @@ def call(Map params) {
     def random = Math.abs(new Random().nextInt() % 1000 + 1)
     def containerName = "${env.JOB_NAME}${env.BUILD_NUMBER}-delete-${random}"
     echo "Loading HA config for deletion"
-    sh "docker cp tests/validation/ha_delete.config ${containerName}:/src/rancher-validation/tests/v3_api/resource"
-    sh "docker run --name ${containerName} -t --env-file .env " +
+    try {
+        sh "docker cp tests/validation/out/ha_delete.config ${containerName}:/src/rancher-validation/tests/v3_api/resource/"
+        sh "docker run --name ${containerName} -t --env-file .env " +
             "${imageName} /bin/bash -c \'" +
             "pytest -v -s --junit-xml=delete_ha.xml --html=reports/delete_ha.html " +
             "-k test_delete_ha_from_config tests/v3_api/\'"
+    } catch(err) {
+        echo "Unable to delete HA installation -- does config file exist?"
+        echo err
+    }
     return containerName
 }
