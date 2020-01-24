f86bfad78838676f28e2e9efe5dc5c6efc4fb0f2
Brandon DePesa <brandon@rancher.com>
2020-01-24 11:43:34 -0700

    update removal of containers

--- vars/removeDockerContainers.groovy
+++ vars/removeDockerContainers.groovy
@@ -2,6 +2,6 @@ def call(def containers) {
     echo "Removing all containers" 
     containers.each { containerName ->
         sh "docker stop ${containerName}"
-        sh "docker rm ${containerName}"
+        sh "docker rm -v ${containerName}"
     }
 }
