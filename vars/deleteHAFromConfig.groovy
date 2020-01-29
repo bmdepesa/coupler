e44e4ce513e408dd20e24ed75b03ce7295b12a1b
Brandon DePesa <brandon@rancher.com>
2020-01-28 18:20:56 -0700

    Update delete HA

--- vars/deleteHAFromConfig.groovy
+++ vars/deleteHAFromConfig.groovy
@@ -1,10 +1,10 @@
 def call(Map params) {
     def random = Math.abs(new Random().nextInt() % 1000 + 1)
     def containerName = "${env.JOB_NAME}${env.BUILD_NUMBER}-delete-${random}"
-    echo "Loading HA config for deletion"
     try {
-        sh "docker cp out/ha_delete.config ${containerName}:/src/rancher-validation/tests/v3_api/resource/"
+        // sh "docker cp out/. ${containerName}:/src/rancher-validation/tests/v3_api/resource/"
         sh "docker run --name ${containerName} -t --env-file .env " +
+            "-v $(pwd)/out:/src/rancher-validation/tests/v3_api/resource/out "
             "${imageName} /bin/bash -c \'" +
             "pytest -v -s --junit-xml=delete_ha.xml --html=reports/delete_ha.html " +
             "-k test_delete_ha_from_config tests/v3_api/\'"
