141cb5eab99c39116afe71f050ef56b3749b4b9f
Brandon DePesa <brandon@rancher.com>
2020-01-28 19:28:07 -0700

    Delete fix

--- vars/deleteHAFromConfig.groovy
+++ vars/deleteHAFromConfig.groovy
@@ -1,11 +1,13 @@
 def call(Map params) {
     def random = Math.abs(new Random().nextInt() % 1000 + 1)
     def containerName = "${env.JOB_NAME}${env.BUILD_NUMBER}-delete-${random}"
+    def imageName = "rancher-validation-${env.JOB_NAME}${env.BUILD_NUMBER}"
+    
     try {
         // sh "docker cp out/. ${containerName}:/src/rancher-validation/tests/v3_api/resource/"
         sh "docker run --name ${containerName} -t --env-file .env " +
             "-v \$(pwd)/out:/src/rancher-validation/tests/v3_api/resource/out " +
-            "${imageName} /bin/bash -c \'" +
+            " /bin/bash -c \'" +
             "pytest -v -s --junit-xml=delete_ha.xml --html=reports/delete_ha.html " +
             "-k test_delete_ha_from_config tests/v3_api/\'"
     } catch(err) {
